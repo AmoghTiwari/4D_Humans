c8226852cba8aef33deebe1203684620da012fda
Amogh Tiwari <amoghtiwari2000@gmail.com>
2023-08-15 15:34:31 +0530

    Refactored convert_pkl_to_pare_fmt.py

--- convert_pkl_to_pare_fmt.py
+++ convert_pkl_to_pare_fmt.py
@@ -1,24 +1,30 @@
-import joblib
-from glob import glob
-# import torch
-import numpy as np
 import os
+import argparse
+import joblib
+import numpy as np
+from glob import glob
 
-input_dir = "outputs/example_data/videos/sample_video_frames"
-output_dir = "outputs/example_data/videos/sample_video_frames_pare_fmt_v02"
+def pkl_reformat_4dh_to_pare(_4dh_pkl):
+    """ Change format of pkl from 4DHumans pkl to PARE"""
+    pare_fmt_pkl = {}
+    pare_fmt_pkl['pred_pose'] = np.concatenate((_4dh_pkl['pred_smpl_params']['body_pose'].cpu().numpy(), _4dh_pkl['pred_smpl_params']['global_orient'].cpu().numpy()), axis=1)
+    pare_fmt_pkl['pred_cam'] = _4dh_pkl['pred_cam'].cpu().numpy()
+    return pare_fmt_pkl
 
-if not os.path.isdir(output_dir):
-    os.makedirs(output_dir)
+if __name__ == "__main__":
+    # input_dir = "outputs/example_data/videos/sample_video_frames"
+    # output_dir = "outputs/example_data/videos/sample_video_frames_pare_fmt_v02"parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--args.input_dir')
+    parser.add_argument('--args.output_dir')
+    args = parser.parse_args()
 
-for _4dh_pkl_paths in glob(f"{input_dir}/*out.pkl"):
-    pare_fmt = {}
-    pkl_fn = "_".join(_4dh_pkl_paths.split("/")[-1].split(".")[:-1])
-    _4dh_out = joblib.load(_4dh_pkl_paths)
-    # _4dh_cam = joblib.load(_4dh_pkl_paths.replace("out.pkl", "cam.pkl"))
-    
-    # concat_v1 = np.concatenate((_4dh_out['pred_smpl_params']['global_orient'].cpu().numpy(), _4dh_out['pred_smpl_params']['body_pose'].cpu().numpy()), axis=1)
-    concat_v2 = np.concatenate((_4dh_out['pred_smpl_params']['body_pose'].cpu().numpy(), _4dh_out['pred_smpl_params']['global_orient'].cpu().numpy()), axis=1)
-    pare_fmt['pred_pose'] = concat_v2
-    pare_fmt['pred_cam'] = _4dh_out['pred_cam'].cpu().numpy()
-    
-    joblib.dump(pare_fmt, os.path.join(output_dir, pkl_fn + ".pkl"))
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    for _4dh_pkl_paths in glob(f"{args.input_dir}/*out.pkl"):
+        pkl_fn = "_".join(_4dh_pkl_paths.split("/")[-1].split(".")[:-1])
+        _4dh_out = joblib.load(_4dh_pkl_paths)
+
+        pare_fmt_pkl = change_format(_4dh_out)
+        joblib.dump(pare_fmt_pkl, os.path.join(args.output_dir, pkl_fn + ".pkl"))
