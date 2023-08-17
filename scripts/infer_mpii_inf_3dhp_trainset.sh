3bb91601a35fe88e1fd11fb827c218fb49751bfd
Amogh Tiwari <amoghtiwari2000@gmail.com>
2023-08-17 09:18:57 +0530

    Modified scripts/infer_mpii_inf_3dhp_trainset.sh to dump echo results to a log file instead of the display

--- scripts/infer_mpii_inf_3dhp_trainset.sh
+++ scripts/infer_mpii_inf_3dhp_trainset.sh
@@ -14,48 +14,49 @@ for subject in $subjects; do
         data_dir=$data_base_dir/$subject/$seq_num/imageSequence
         for vid_fp in $data_dir/*; do 
             #vid_fp=(See above)
-            echo Processing: $vid_fp
-            echo 
+            echo Processing: $vid_fp >> log_infer_mpi_inf_3dhp.txt
+            echo >> log_infer_mpi_inf_3dhp.txt
             vid_fbn=${vid_fp/.avi//}
-            echo vid_fbn: $vid_fbn
+            echo vid_fbn: $vid_fbn >> log_infer_mpi_inf_3dhp.txt
             vid_bn=(${vid_fbn//\// })
             vid_bn=${vid_bn[-1]}
-            echo vid_bn: $vid_bn
-            echo 
+            echo vid_bn: $vid_bn >> log_infer_mpi_inf_3dhp.txt
+            echo  >> log_infer_mpi_inf_3dhp.txt
 
             ##### FFMPEG BLOCK #####
             mkdir $vid_fbn
             conda activate mps_env
             ffmpeg_start_dt=$(date)
-            echo "Running ffmpeg, start time: $ffmpeg_start_dt"
-            ffmpeg -i $vid_fp -r 30000/1001 -f image2 -v error $vid_fbn/%06d.jpg
+            echo "Running ffmpeg, start time: $ffmpeg_start_dt" >> log_infer_mpi_inf_3dhp.txt
+            # ffmpeg -i $vid_fp -r 30000/1001 -f image2 -v error $vid_fbn/%06d.jpg
             ffmpeg_end_dt=$(date)
-            echo "Finished ffmpeg, end time: $ffmpeg_end_dt"
+            echo "Finished ffmpeg, end time: $ffmpeg_end_dt" >> log_infer_mpi_inf_3dhp.txt
             num_frames=$(ls $vid_fbn | wc -l)
-            echo "Processed $num_frames frames"
-            echo
+            echo "Processed $num_frames frames" >> log_infer_mpi_inf_3dhp.txt
+            echo >> log_infer_mpi_inf_3dhp.txt
             conda deactivate
             ##### FFMPEG BLOCK #####
             
             inference_start_dt=$(date)
-            echo "Running inference, start time: $inference_start_dt"
-            python demo_w_pkl.py --img_folder $vid_fbn --out_folder outputs/mpi_inf_3dhp/$subject/$seq_num/$vid_bn --batch_size=48 --save_pkl
+            echo "Running inference, start time: $inference_start_dt" >> log_infer_mpi_inf_3dhp.txt
+            # python demo_w_pkl.py --img_folder $vid_fbn --out_folder outputs/mpi_inf_3dhp/$subject/$seq_num/$vid_bn --batch_size=48 --save_pkl
             inference_end_dt=$(date)
-            echo "Finished inference, start time: $inference_end_dt"
-            echo Processed $(ls $vid_fbn | wc -l) frames
-            echo
-            echo "Removing $vid_fbn"
+            echo "Finished inference, end time: $inference_end_dt" >> log_infer_mpi_inf_3dhp.txt
+            echo Processed $(ls $vid_fbn | wc -l) frames >> log_infer_mpi_inf_3dhp.txt
+            echo >> log_infer_mpi_inf_3dhp.txt
+            echo "Removing $vid_fbn" >> log_infer_mpi_inf_3dhp.txt
             rm -r $vid_fbn
 
-            echo "########## ##########"
-            echo "STATISTICS"
-            echo "########## ##########"
-            echo "ffmpeg start time: $ffmpeg_start_dt"
-            echo "ffmpeg end time: $ffmpeg_end_dt"
-            echo "inference start time: $inference_start_dt"
-            echo "inference start time: $inference_end_dt"
-            echo Processed $num_frames frames
-            echo
+            echo "########## ##########" >> log_infer_mpi_inf_3dhp.txt
+            echo "STATISTICS" >> log_infer_mpi_inf_3dhp.txt
+            echo "########## ##########" >> log_infer_mpi_inf_3dhp.txt
+            echo "ffmpeg start time: $ffmpeg_start_dt" >> log_infer_mpi_inf_3dhp.txt
+            echo "ffmpeg end time: $ffmpeg_end_dt" >> log_infer_mpi_inf_3dhp.txt
+            echo "inference start time: $inference_start_dt" >> log_infer_mpi_inf_3dhp.txt
+            echo "inference end time: $inference_end_dt" >> log_infer_mpi_inf_3dhp.txt
+            echo Processed $num_frames frames >> log_infer_mpi_inf_3dhp.txt
+            echo >> log_infer_mpi_inf_3dhp.txt
+            echo >> log_infer_mpi_inf_3dhp.txt
             exit 0
         done
     done
