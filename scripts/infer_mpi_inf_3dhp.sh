a5ef70afa617ced6dddb6e45447fb796d74f68c3
Amogh Tiwari <amoghtiwari2000@gmail.com>
2023-08-17 15:28:57 +0530

    Modified scripts/infer_mpi_inf_3dhp.sh to work on a list of videos instead of all vids in a directory AND made some changes to logger format

--- scripts/infer_mpi_inf_3dhp.sh
+++ scripts/infer_mpi_inf_3dhp.sh
@@ -5,18 +5,29 @@ eval "$(conda shell.bash hook)"
 conda activate 4D_humans_env
 cd ../
 
-vid_fp=$data_dir/video_1.avi
+
+run_start_dt=$(date)
+echo "############################## ##############################" >> scripts/log_infer_mpi_inf_3dhp.txt
+echo "Present run started at: $run_start_dt" >> scripts/log_infer_mpi_inf_3dhp.txt
+echo "############################## ##############################" >> scripts/log_infer_mpi_inf_3dhp.txt
 
 subjects=("S1")
 seq_nums=("Seq1")
-vid_name=("video_0.avi")
-for subject in $subjects; do
-    for seq_num in $seq_nums; do
+vid_names=(video_2.avi video_4.avi)
+echo "Subjects: ${subjects[@]}"  >> scripts/log_infer_mpi_inf_3dhp.txt
+echo "Sequences: ${seq_nums[@]}"  >> scripts/log_infer_mpi_inf_3dhp.txt
+echo "vid_names: ${vid_names[@]}"  >> scripts/log_infer_mpi_inf_3dhp.txt
+
+for subject in ${subjects[@]}; do
+    for seq_num in ${seq_nums[@]}; do
         data_dir=$data_base_dir/$subject/$seq_num/imageSequence
         # for vid_fp in $data_dir/*; do 
-        for vid_fp in $data_base_dir/$subject/$seq_num/imageSequence/$vid_name; do 
+        for vid_name in ${vid_names[@]}; do
             #vid_fp=(See above)
+            vid_fp=$data_base_dir/$subject/$seq_num/imageSequence/$vid_name
+            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp.txt
             echo Processing: $vid_fp >> scripts/log_infer_mpi_inf_3dhp.txt
+            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp.txt
             echo >> scripts/log_infer_mpi_inf_3dhp.txt
             vid_fbn=${vid_fp/.avi//}
             echo vid_fbn: $vid_fbn >> scripts/log_infer_mpi_inf_3dhp.txt
@@ -30,7 +41,7 @@ for subject in $subjects; do
             conda activate mps_env
             ffmpeg_start_dt=$(date)
             echo "Running ffmpeg, start time: $ffmpeg_start_dt" >> scripts/log_infer_mpi_inf_3dhp.txt
-            # ffmpeg -i $vid_fp -r 30000/1001 -f image2 -v error $vid_fbn/%06d.jpg
+            ffmpeg -i $vid_fp -r 30000/1001 -f image2 -v error $vid_fbn/%06d.jpg
             ffmpeg_end_dt=$(date)
             echo "Finished ffmpeg, end time: $ffmpeg_end_dt" >> scripts/log_infer_mpi_inf_3dhp.txt
             num_frames=$(ls $vid_fbn | wc -l)
@@ -41,7 +52,7 @@ for subject in $subjects; do
             
             inference_start_dt=$(date)
             echo "Running inference, start time: $inference_start_dt" >> scripts/log_infer_mpi_inf_3dhp.txt
-            # python demo_w_pkl.py --img_folder $vid_fbn --out_folder outputs/mpi_inf_3dhp/$subject/$seq_num/$vid_bn --batch_size=48 --save_pkl
+            python demo_w_pkl.py --img_folder $vid_fbn --out_folder outputs/mpi_inf_3dhp/$subject/$seq_num/$vid_bn --batch_size=48 --save_pkl
             inference_end_dt=$(date)
             echo "Finished inference, end time: $inference_end_dt" >> scripts/log_infer_mpi_inf_3dhp.txt
             echo Processed $(ls $vid_fbn | wc -l) frames >> scripts/log_infer_mpi_inf_3dhp.txt
@@ -59,7 +70,7 @@ for subject in $subjects; do
             echo Processed $num_frames frames >> scripts/log_infer_mpi_inf_3dhp.txt
             echo >> scripts/log_infer_mpi_inf_3dhp.txt
             echo >> scripts/log_infer_mpi_inf_3dhp.txt
-            exit 0
+            # exit 0
         done
     done
 done
