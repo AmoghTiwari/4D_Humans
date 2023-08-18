a42c642be6c7e1cee48851b748023fcfe5419c26
Amogh Tiwari <amoghtiwari2000@gmail.com>
2023-08-18 17:54:27 +0530

    Modified bash inference scripts on groot

--- scripts/infer_mpii_3dhp_groot_g2.sh
+++ scripts/infer_mpii_3dhp_groot_g2.sh
@@ -7,16 +7,16 @@ cd ../
 
 
 run_start_dt=$(date)
-echo "############################## ##############################" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-echo "Present run started at: $run_start_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-echo "############################## ##############################" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+echo "############################## ##############################" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+echo "Present run started at: $run_start_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+echo "############################## ##############################" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
 
 subjects=("S1")
-seq_nums=("Seq1" "Seq2")
-vid_names=(video_8.avi)
-echo "Subjects: ${subjects[@]}"  >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-echo "Sequences: ${seq_nums[@]}"  >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-echo "vid_names: ${vid_names[@]}"  >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+seq_nums=("Seq2")
+vid_names=(video_7.avi)
+echo "Subjects: ${subjects[@]}"  >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+echo "Sequences: ${seq_nums[@]}"  >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+echo "vid_names: ${vid_names[@]}"  >> scripts/log_infer_mpii_3dhp_groot_g2.txt
 
 for subject in ${subjects[@]}; do
     for seq_num in ${seq_nums[@]}; do
@@ -25,51 +25,51 @@ for subject in ${subjects[@]}; do
         for vid_name in ${vid_names[@]}; do
             #vid_fp=(See above)
             vid_fp=$data_base_dir/$subject/$seq_num/imageSequence/$vid_name
-            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo Processing: $vid_fp >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "########## ##########" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo Processing: $vid_fp >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "########## ##########" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             vid_fbn=${vid_fp/.avi//}
-            echo vid_fbn: $vid_fbn >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo vid_fbn: $vid_fbn >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             vid_bn=(${vid_fbn//\// })
             vid_bn=${vid_bn[-1]}
-            echo vid_bn: $vid_bn >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo  >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo vid_bn: $vid_bn >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo  >> scripts/log_infer_mpii_3dhp_groot_g2.txt
 
             ##### FFMPEG BLOCK #####
             mkdir $vid_fbn
             conda activate mps_env
             ffmpeg_start_dt=$(date)
-            echo "Running ffmpeg, start time: $ffmpeg_start_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "Running ffmpeg, start time: $ffmpeg_start_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             ffmpeg -i $vid_fp -r 30000/1001 -f image2 -v error $vid_fbn/%06d.jpg
             ffmpeg_end_dt=$(date)
-            echo "Finished ffmpeg, end time: $ffmpeg_end_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "Finished ffmpeg, end time: $ffmpeg_end_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             num_frames=$(ls $vid_fbn | wc -l)
-            echo "Processed $num_frames frames" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "Processed $num_frames frames" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             conda deactivate
             ##### FFMPEG BLOCK #####
             
             inference_start_dt=$(date)
-            echo "Running inference, start time: $inference_start_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "Running inference, start time: $inference_start_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             python demo_w_pkl.py --img_folder $vid_fbn --out_folder outputs/mpi_inf_3dhp/$subject/$seq_num/$vid_bn --batch_size=48 --save_pkl
             inference_end_dt=$(date)
-            echo "Finished inference, end time: $inference_end_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo Processed $(ls $vid_fbn | wc -l) frames >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "Removing $vid_fbn" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "Finished inference, end time: $inference_end_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo Processed $(ls $vid_fbn | wc -l) frames >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "Removing $vid_fbn" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             rm -r $vid_fbn
 
-            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "STATISTICS" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "########## ##########" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "ffmpeg start time: $ffmpeg_start_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "ffmpeg end time: $ffmpeg_end_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "inference start time: $inference_start_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo "inference end time: $inference_end_dt" >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo Processed $num_frames frames >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
-            echo >> scripts/log_infer_mpi_inf_3dhp_groot_gpu2.txt
+            echo "########## ##########" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "STATISTICS" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "########## ##########" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "ffmpeg start time: $ffmpeg_start_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "ffmpeg end time: $ffmpeg_end_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "inference start time: $inference_start_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo "inference end time: $inference_end_dt" >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo Processed $num_frames frames >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo >> scripts/log_infer_mpii_3dhp_groot_g2.txt
+            echo >> scripts/log_infer_mpii_3dhp_groot_g2.txt
             # exit 0
         done
     done
