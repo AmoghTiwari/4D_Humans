fbc038d061ac4e4bb09e3e8a685dad60f8be9b66
Amogh Tiwari <amoghtiwari2000@gmail.com>
2023-08-18 13:10:58 +0530

    Edited bash inference scripts - gnode-65

--- scripts/infer_mpii_3dhp_gnode65_g0.sh
+++ scripts/infer_mpii_3dhp_gnode65_g0.sh
@@ -24,8 +24,9 @@ echo "Present run started at: $run_start_dt" >> scripts/log_infer_mpii_3dhp_gnod
 echo "############################## ##############################" >> scripts/log_infer_mpii_3dhp_gnode65_g0.txt
 
 subjects=("S2")
-seq_nums=("Seq1")
-vid_names=(video_0.avi video_1.avi)
+seq_nums=("Seq2")
+# vid_names=(video_0.avi video_1.avi)
+vid_names=(video_0.avi)
 echo "Subjects: ${subjects[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g0.txt
 echo "Sequences: ${seq_nums[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g0.txt
 echo "vid_names: ${vid_names[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g0.txt
--- scripts/infer_mpii_3dhp_gnode65_g3.sh
+++ scripts/infer_mpii_3dhp_gnode65_g3.sh
@@ -24,8 +24,9 @@ echo "Present run started at: $run_start_dt" >> scripts/log_infer_mpii_3dhp_gnod
 echo "############################## ##############################" >> scripts/log_infer_mpii_3dhp_gnode65_g3.txt
 
 subjects=("S2")
-seq_nums=("Seq1")
-vid_names=(video_7.avi video_8.avi)
+seq_nums=("Seq2")
+# vid_names=(video_7.avi video_8.avi)
+vid_names=(video_7.avi)
 echo "Subjects: ${subjects[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g3.txt
 echo "Sequences: ${seq_nums[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g3.txt
 echo "vid_names: ${vid_names[@]}"  >> scripts/log_infer_mpii_3dhp_gnode65_g3.txt
